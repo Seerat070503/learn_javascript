fd5a84e337f405ce255864d8c3d84bba0b2be2a8
Manseerat657 <manseerat1756.be21@chitkara.edu.in>
2024-03-05 01:34:12 +0530

    commited again

--- strings.js
+++ strings.js
@@ -9,4 +9,14 @@ let val3 =1+2 +"2"; //reult in adition and then string
 let val4 =2+"5"+6; // result in string
 let val6 = "5"+8+5; // results in string
 
-console.table([str3,value1,value2,val3,val4,val6]);
+let val = true+3;
+
+variable =true+ // gives the error
+// infinity * infinity --> infinity
+// infinity * 0 = NaN
+
+console.table([str3,value1,value2,val3,val4,val6,val]);
+console.log(3/2);
+console.log(3**2) // 3 to the power 2
+console.log(1/0); // infinity 
+console.log(1*Infinity); // infinity

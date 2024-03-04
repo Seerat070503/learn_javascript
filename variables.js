51ff96a379fc720ae1c5b5e828c8bc44cb399284
Manseerat657 <manseerat1756.be21@chitkara.edu.in>
2024-03-04 14:36:22 +0530

    commit

---
+++ variables.js
@@ -0,0 +1,21 @@
+let i = 10;
+const val = 30;
+let text ="hello how are you";
+
+// we cannot chane the const in js 
+// NOTE 
+
+// var is of global scope and let is used for local scope
+
+// let txt = hello; this line will give you error 
+text = "why";
+var x= 20.8888;
+
+console.table([i,val,text,x]);
+
+/* 
+try not to use 
+VAR intead use let 
+and put string inside ""(double quotes);
+else it will give error
+*/

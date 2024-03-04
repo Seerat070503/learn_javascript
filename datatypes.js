15fee3abd6855f2dc9234ac12dd48805586f2231
Manseerat657 <manseerat1756.be21@chitkara.edu.in>
2024-03-04 14:56:58 +0530

    commit

---
+++ datatypes.js
@@ -0,0 +1,13 @@
+let num = 100;
+let val ="Hello";
+let obj =null;
+let bool =false;
+
+let value;
+
+console.table([num,val,obj,value,bool]);
+console.log(typeof(num));
+console.log(typeof(obj));
+console.log(typeof(val));
+console.log(typeof(20.8)); // the float or double value is treated as number in js
+console.log(typeof(bool));
